13270d3df9333a5ec33e21daff8bd3bbca33d983
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-04 11:36:45 -0300

    aula 11

--- aula11.py
+++ aula11.py
@@ -1,1 +1,12 @@
-#Condições Aninhadas
+# Estruturas condicionais aninhadas
+
+nome = str(input('Informe seu primeiro nome:'))
+
+if nome == 'João':
+    print('Seu nome é bem popular no Brasil!')
+elif nome == 'Pedro' or nome == 'Tiago' or nome == 'Lucas':
+    print('Seu nome também é bastante popular!')
+elif nome in 'Paula, Jéssica, Jade, Jude, Milena':
+    print('Olá {}, Tens um belo nome feminino!'.format(nome))
+else:
+    print('Olá {}, talvez seu nome não seja tão popular!'.format(nome))
