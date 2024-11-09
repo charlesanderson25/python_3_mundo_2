c03cf37d6220abc6bc9588fd9b2e8ccaf4a0aa8b
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-09 10:18:35 -0300

    Adiciona Count

--- ex048.py
+++ ex048.py
@@ -1,8 +1,10 @@
 # Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 
 soma = 0
+count = 0
 
-for c in range(1, 500):
-    if c % 2 != 0:
+for c in range(1, 501):
+    if c % 2 != 0 and c % 3 == 0:
+        count = count + 1
         soma = soma + c
-print('A soma dos números ímpares entre 1 e 500 é de: {}'.format(soma))
+print('A soma de todos os {} números ímpares e múltiplos de 3 entre 1 e 500 é de: {}'.format(count, soma))
