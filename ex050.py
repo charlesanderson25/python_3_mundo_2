ac88cf69ffe76b1f345ce8616589d1c4c45e9640
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-09 12:06:26 -0300

    Add count

--- ex050.py
+++ ex050.py
@@ -1,11 +1,13 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
 soma = 0
+count = 0
 
 for c in range(1, 7):
     n = int(input('Informe seis números inteiros:'))
     if n % 2 == 0:
+        count = count + 1
         soma = soma + n
     else:
         print('Os números ímpares digitados, serão desconsiderados!')
-print('A soma dos números pares informados é de: {}'.format(soma))
+print('A soma dos {} números pares informados é de: {}'.format(count, soma))
