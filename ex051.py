433d3ff8c0c5012f7c304ab57f80afda0de787f6
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-09 10:06:51 -0300

    Progressão aritmética

---
+++ ex051.py
@@ -0,0 +1,10 @@
+# Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão. 
+
+p1 = int(input('Informe o primeiro termo da Progressão Aritmética(PA):'))
+razao = int(input('Informe a razão da Progressão Aritmética(PA):'))
+
+print('O primeiro termo da razão é: {}'.format(p1))
+
+for c in range(1, 11):
+    p2 =
+
