7b4d5faec6879aa0f1fe1669559e4248adc86926
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-15 10:50:38 -0300

    Adiciona exemplos

--- aula13.py
+++ aula13.py
@@ -56,3 +56,50 @@ for c in range(0, 4):
     s = s + n
 print('O somatório dos valores informados é de {}'.format(s))
 print('FIM!!')
+
+# Laços de repetição - Parte 2
+
+for c in range(1, 6):
+    print('Oi')
+print('FIM')
+
+#-------------------------------------------------#
+
+for c in range(1, 6):
+    print(c)
+print('FIM')
+
+#-------------------------------------------------#
+
+for c in range(6, 0):
+        print(c)
+print('FIM')
+
+#-------------------------------------------------#
+
+for c in range(6, 0, -1):
+        print(c)
+print('Fim')
+
+#-------------------------------------------------#
+
+for c in range(0, 6, 2):
+        print(c)
+print('Fim')
+
+#-------------------------------------------------#
+
+n = int(input('Digite um número:'))
+for c in range(0, n + 1):
+    print(c)
+print('FIM')
+    
+#-------------------------------------------------#
+
+i = int(input('Início:'))
+f = int(input('Fim:'))
+p = int(input('Passo:'))
+
+for c in range(i, f, p):
+      print(c)
+print('FIM!!!')
