cd034541a98b41f549c1121478c5c8aecc8a5256
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-17 17:23:03 -0300

    Sequência Fibonacci

--- ex063.py
+++ ex063.py
@@ -2,11 +2,26 @@
 
 # Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8
 
+print('-=' * 20)
+print('Sequência de Fibonacci')
+print('-=' * 20)
+
 n = int(input('Quantos termos deseja mostrar?'))
 primeiro = 0
 segundo = 1
 
-calculo = 
+print('{} > {}'.format(primeiro, segundo), end='')
+
+count = 3
+
+while count <= n:
+    terceiro = primeiro + segundo
+    print(' > {}'.format(terceiro), end='')
+    primeiro = segundo
+    segundo = terceiro
+    count = count + 1
+print(' > Fim')
+
 
 # NÃO CONCLUÍDO!
 # NÃO CONCLUÍDO!
