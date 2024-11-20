607169ab1798eb21ff5f3d07078a6d621d1b866e
charlesanderson25 <charles_spfc@hotmail.com>
2024-11-20 19:14:34 -0300

    Interromper while com break

--- aula15.py
+++ aula15.py
@@ -1,1 +1,12 @@
 # Laços de Repetição - Interrompendo repetições While
+
+n = 0
+soma = 0
+
+while True:
+    n = int(input('Informe um número inteiro:'))
+    if n == 999:
+        break
+    soma = soma + n
+# print('A soma dos números informados é de: {}'.format(soma))
+print(f'A soma dos números informados é de: {soma}')
